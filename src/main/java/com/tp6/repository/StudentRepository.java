ebe1f92478f82af804449110f7a197d391f1efba
Chekroun Adnan <adnanchekroun00@gmail.com>
2025-10-27 14:32:56 +0100

    first commit

---
+++ src/main/java/com/tp6/repository/StudentRepository.java
@@ -0,0 +1,19 @@
+package com.tp6.repository;
+
+import java.util.Collection;
+
+import com.tp6.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    // Recherche d'un étudiant par son identifiant
+    Student findById(int id);
+
+    // Requête personnalisée pour compter les étudiants par année de naissance
+    @Query("SELECT YEAR(s.dateNaissance), COUNT(s) FROM Student s GROUP BY YEAR(s.dateNaissance)")
+    Collection<Object[]> findNbrStudentByYear();
+}
